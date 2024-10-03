8ac9c11403c003426c103d8ad505aaffe4b3f18f
h3h3h0h0 <deboss.wt@gmail.com>
2024-10-03 17:35:05 -0400

    wrote session manager, added few helper functions as well

--- src/backend/src/session.py
+++ src/backend/src/session.py
@@ -1,13 +1,51 @@
 import json
 import uuid #we will use uuid5 in this game
 from enum import Enum
+from names_generator import generate_name
+import time
 # manages player sessions, allowing for various game modes and scoring systems
 defaultfile = './scoringconfig'
 
 # configuration: dict containing all the configuration details of the current session
 # score: the current score
+
+def fetchAccountDetails(): #TODO: fetch account details from database
+    return {"displayname": "DISPLAY NAME PLACEHOLDER"}
+
+def getActionStats(mode, action, configfile=defaultfile): #return the score and rounds left change of an action, returns 0, 0 (do nothing) if errored
+    configdict = None
+    with open(configfile) as cfg:
+        configdict = json.load(cfg)
+    if configdict is None:
+        return 0, 0
+    if not mode in configdict.keys():
+        return 0, 0
+    current = configdict["mode"]
+    if not "actions" in current:
+        return 0, 0
+    current = current["actions"]
+    if len(action) == 0: #we can use this to refresh without changing anything
+            return 0, 0
+    lexemes = action.split()
+    for lex in lexemes:
+        if isinstance(current, dict): # parse the current lexeme as string
+            if not lex in current.keys():
+                return 0, 0
+            current = current[lex]
+        elif isinstance(current, list):
+            try:
+                current = current[int(lex)]
+            except ValueError:
+                return 0, 0
+        else: #something has really gone wrong here
+            return 0, 0
+    if (not isinstance(current, list)) or (not len(current) == 2): # the leaf object is a list of length 2, representing (score change, round change)
+        return 0, 0
+    return int(current[0]), int(current[1])
+
 class Session:
     def __init__(self, mode, configfile=defaultfile, auid="", displayname="Guest"):
+        self.cfile = configfile
         self.auid = auid #if this is empty, then guest session
         self.sessionuuid = str(uuid.uuid5())
         self.displayname = displayname
@@ -96,6 +134,9 @@ class Session:
     
     def getMode(self):
         return self.mode
+    
+    def getCFile(self):
+        return self.cfile
 
 class GroupStatus(Enum):
     NORMAL = 0
@@ -105,24 +146,34 @@ class Group: #multiplayer support, each session is contained in a group of one o
     def __init__(self, initiator): #initiator is the session that initiates the group, will "own" the group
         self.groupid = str(uuid.uuid5())
         self.mode = initiator.getMode()
+        self.cfile = initiator.getCFile()
         self.players = {initiator.getUUID(): initiator}
         self.playerids = [initiator.getUUID()] #the owner will always sit at index 0, if they leave then ownership goes to the next available person
                                     #I do not trust trying to index dict keys
         self.status = GroupStatus.NORMAL
         self.unfinished = 1
+        self.lastaccess = time.time()
     def addPlayer(self, player): #add player session
+        self.lastaccess = time.time()
         if not self.players.get(player.getUUID()) is None:
             return False #DO NOT add duplicates
-        if self.mode != player.getMode():
-            return False #DO NOT mix modes
+        if self.mode != player.getMode() or self.cfile != player.getCFile():
+            if self.status == GroupStatus.EMPTY:
+                self.mode = player.getMode()
+                self.cfile = player.getCFile()
+            else:
+                return False #DO NOT mix modes, of course if mode is empty that is different
         self.players[player.getUUID] = player
         self.playerids.append(player.getUUID)
         if not player.isGameOver():
             self.unfinished += 1
             self.status = GroupStatus.NORMAL
+        elif self.unfinished == 0:
+            self.status = GroupStatus.FINISHED
         return True
 
     def removePlayer(self, uuid):
+        self.lastaccess = time.time()
         if self.players.get(uuid) is None:
             return False
         else:
@@ -137,15 +188,19 @@ class Group: #multiplayer support, each session is contained in a group of one o
             return True
     
     def getStatus(self):
+        self.lastaccess = time.time()
         return self.status
     
     def getNPlayers(self):
+        self.lastaccess = time.time()
         return len(self.playerids)
     
     def isOwner(self, uuid):
+        self.lastaccess = time.time()
         return len(self.playerids) > 0 and uuid == self.playerids[0]
     
     def doUpdate(self, uuid, action):
+        self.lastaccess = time.time()
         if self.players.get(uuid) is None:
             return False
         stat = self.players.get(uuid).update(action)
@@ -156,14 +211,62 @@ class Group: #multiplayer support, each session is contained in a group of one o
             if self.unfinished == 0:
                 self.status = GroupStatus.FINISHED
         return True
+    
     def getState(self):
+        self.lastaccess = time.time()
         statedict = {}
         for id in self.playerids:
             statedict[id] = self.players[id].getState()
         return statedict
+    
+    def getID(self):
+        self.lastaccess = time.time()
+        return self.groupid
+    
+    def getCFile(self):
+        self.lastaccess = time.time()
+        return self.cfile
+    
+    def getMode(self):
+        self.lastaccess = time.time()
+        return self.mode
+    
+    def getSession(self, uuid): #use if you really want to use the session methods
+        return self.players.get(uuid)
 
 class SessionManager:
     def __init__(self):
         self.groups = {} #these are indexed by group ID
-        self.group_ref = {} #session ID -> group ID
-        self.accounts = {} #account -> session ID (also used to prevent multiple sessions on the same account)
+    def start(self, mode, configfile="", account=""): #initiates group, returns group ID which may be empty if there is an error
+        initsession = None
+        if account != "":
+            initsession = Session(mode, configfile, account, fetchAccountDetails(account)["displayname"])
+        else:
+            guestname = generate_name(style='capital') # using name generator, generate a randomized guest name
+            initsession = Session(mode, configfile, account, guestname)
+        group = Group(initsession)
+        self.groups[group.getID()] = group
+        return group.getID()
+    def addUser(self, groupid, account=""): #returns status
+        if self.groups.get(groupid) is None:
+            return False
+        newsession = None
+        if account != "":
+            newsession = Session(self.groups[groupid].getMode(), self.groups[groupid].getCFile(), account, fetchAccountDetails(account)["displayname"])
+        else:
+            guestname = generate_name(style='capital') # using name generator, generate a randomized guest name
+            newsession = Session(self.groups[groupid].getMode(), self.groups[groupid].getCFile(), account, guestname)
+        return self.groups[groupid].addPlayer(newsession)
+    
+    def addSession(self, groupid, session):
+        if self.groups.get(groupid) is None:
+            return False
+        return self.groups[groupid].addPlayer(session)
+    
+    def removeSession(self, groupid, uuid):
+        if self.groups.get(groupid) is None:
+            return False
+        return self.groups[groupid].removePlayer(uuid)
+    
+    def getGroup(self, groupid): #if you wanna do some operation on the group using group methods, use this
+        return self.groups[groupid]

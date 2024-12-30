7707e3b8d3c4ce85d2ee1f56241eda627581e86c
Eric <33041238+erickang21@users.noreply.github.com>
2024-12-30 10:27:16 -0500

    no debug

--- backend/src/api.py
+++ backend/src/api.py
@@ -256,3 +256,4 @@ if __name__ == '__main__':
     #app.run(debug=True, port=5001)
     from waitress import serve
     serve(app, host="127.0.0.1", port=5000)
+    print("[PROD] Hosting on PORT=5000")
